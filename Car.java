5c92df214270d2333423785728856bf94b9019c3
Michael Zhang <mzhang26@students.polytechnic.org>
2024-02-20 20:40:11 -0800

    changed car mutators

--- Car.java
+++ Car.java
@@ -1,24 +1,50 @@
 public class Car {
     private int revenue;
 
-    private int destinationStation;
     private int position;
+    private int destinationStation;
     private boolean increasingPos;
+
+    private int roadLength;
 
     public Car(int stationCount) {
         revenue = 0;
 
-        destinationStation = (int) (Math.random() * stationCount);
-        position = (int) (Math.random() * stationCount);
+        changePosition((int) (Math.random() * stationCount));
+        changeDestinationStation((int) (Math.random() * stationCount));
         
         alignIncreasingPosVal();
+
+        roadLength = stationCount;
     }
 
     private void alignIncreasingPosVal() {
         increasingPos = position < destinationStation;
     }
 
+    public void changePosition(int newPos) {
+        position = newPos;
+
+        if (position == destinationStation)
+            getNewDestination();
+
+    }
+
+    public void changeDestinationStation(int newPos) {
+        position = newPos;
+
+        if (position == destinationStation) 
+            getNewDestination();
+
+    }
+
+    private void getNewDestination() {
+        destinationStation = (int) (Math.random() * roadLength);
+    }
+
     public int getRevenue() { return revenue; }
+
+    public int getPosition() { return position; }
 
     public int getDestinationStation() { return destinationStation; }
 
