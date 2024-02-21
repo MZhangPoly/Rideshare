0a37c5deb93957d9b0f2ce252b0b0633eb04a68b
Michael Zhang <mzhang26@students.polytechnic.org>
2024-02-20 21:22:20 -0800

    added rideshare tester

---
+++ RideshareTester.java
@@ -0,0 +1,16 @@
+public class RideshareTester {
+    public static void main(String[] args) {
+        int ticks = 5;
+
+        Road road = new Road(2, 3, 5);
+
+        System.out.println("\n Starting simulation... \n ");
+
+        for (int i = 0; i < ticks; i++) {
+            road.tick();
+
+            System.out.println(" --------------- tick " + i + " --------------- \n");
+            System.out.println(road);
+        }
+    }
+}
