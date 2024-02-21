2a2c1d1d13caebc4a5b8578076d45bc5cb06924c
Michael Zhang <mzhang26@students.polytechnic.org>
2024-02-20 21:55:59 -0800

    added tostring()

--- Road.java
+++ Road.java
@@ -60,7 +60,7 @@ public class Road {
 
     public ArrayList<Passenger> getPassengers() { return passengers; }
 
-    private int getCarsOnMile(int mile) {
+    private int getCarCountOnMile(int mile) {
         int carsOnMile = 0;
 
         for (Car car : cars) 
@@ -70,7 +70,7 @@ public class Road {
         return carsOnMile;
     }
 
-    private int getPassengersOnMile(int mile) {
+    private int getPassengerCountOnMile(int mile) {
         int passengersOnMile = 0;
 
         for (Passenger passenger : passengers) 
@@ -80,11 +80,41 @@ public class Road {
         return passengersOnMile;
     }
 
+    private ArrayList<Car> getCarsOnMile(int mile) {
+        ArrayList<Car> carsOnMile = new ArrayList<Car>();
+
+        for (Car car : cars)
+            if (car.getPosition() == mile)
+                carsOnMile.add(car);
+        
+        return carsOnMile;
+    }
+
+    private ArrayList<Passenger> getPassengersOnMile(int mile) {
+        ArrayList<Passenger> passengersOnMile = new ArrayList<Passenger>();
+
+        for (Passenger passenger : passengers)
+            if (passenger.getPosition() == mile)
+                passengersOnMile.add(passenger);
+        
+        return passengersOnMile;
+    }
+
     public String toString() {
         String str = "";
 
         for (int i = 0; i < length; i++) {
-            str += "Mile " + i + ": cars - " + getCarsOnMile(i) + " | passengers - " + getPassengersOnMile(i) + "\n";
+            str += "Mile " + i + ": cars - " + getCarCountOnMile(i) + " | passengers - " + getPassengerCountOnMile(i) + " ||| ";
+            
+            for (Car car : getCarsOnMile(i)) {
+                str += car;
+            }
+
+            for (Passenger passenger : getPassengersOnMile(i)) {
+                str += passenger;
+            }
+
+            str += "\n";
         }
 
         return str;
